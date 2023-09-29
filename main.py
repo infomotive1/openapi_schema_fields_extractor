443125945690d6890b2b96a2e7c79253d1f5d02f
infomotive1 <41651423+infomotive1@users.noreply.github.com>
2023-09-29 17:12:26 +1000

    Updates and new test file

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import yaml
 import csv
 from io import StringIO
+# import yaml.resolver
 # import re
+
+INPUT_DIR = '.\\input\\'
+OUTPUT_DIR = '.\\output\\'
+LOG_FILE = '.log'
+log_contents = []
+
+WRITE_LOG = True
+PRINT_TO_TERMINAL = True
+
+# def custom_boolean_resolver(self, node):
+#     # Return True for any input
+#     return True
+
+# # Register the custom resolver
+# yaml.resolver.BaseResolver.yaml_implicit_resolvers['tag:yaml.org,2002:bool'] = [custom_boolean_resolver]
 
 
 def get_components_schemas(spec_data):
@@ -10,7 +26,7 @@ def get_components_schemas(spec_data):
     else:
         return 0
 
-
+# Function to determine if $ref is a local reference 
 def resolve_ref(spec, ref):
     parts = ref.split('/')
     for part in parts:
@@ -20,12 +36,7 @@ def resolve_ref(spec, ref):
         spec = spec[part]
     return spec
 
-
-class NoAliasDumper(yaml.Dumper):
-    def ignore_aliases(self, data):
-        return True
-
-
+# Function to recursively resolve local inline schema references 
 def resolve_references(spec, root):
     if isinstance(spec, dict):
         if '$ref' in spec:
@@ -45,26 +56,7 @@ def resolve_references(spec, root):
             spec[index] = resolve_references(item, root)
     return spec
 
-
-# def resolve_references(spec, root):
-#     """Recursively resolve references in the spec."""
-#     if isinstance(spec, dict):
-#         if '$ref' in spec:
-#             ref_value = spec['$ref']
-#             # Extract the key (e.g., MailingAddress from '#/components/schemas/MailingAddress')
-#             ref_key = ref_value.split('/')[-1]
-#             refs_encountered.add(ref_key)
-#             # Replace the current dict with the resolved ref
-#             return resolve_references(resolve_ref(root, ref_value), root)
-#         for key, value in spec.items():
-#             spec[key] = resolve_references(value, root)
-#     elif isinstance(spec, list):
-#         for index, item in enumerate(spec):
-#             spec[index] = resolve_references(item, root)
-#     return spec
-
-
-
+#  Function to remove referenced schemas from the schemas set
 def remove_reference_sources(resolved_data, refs_encountered):
     for ref_key in refs_encountered:
         del resolved_data['components']['schemas'][ref_key]
@@ -73,64 +65,72 @@ def remove_reference_sources(resolved_data, refs_encountered):
 # Extract schema properties (ie. data fields), format, return property and its attributes
 def extract_fields_from_schema(schema, components, parent_name=''):
     fields = []
+    print(f'\nschema: {schema}')
 
     if 'properties' in schema:
         required_properties = schema.get('required', [])
         for property_name, attributes in schema['properties'].items():
-            # combine various attribute definitions to the format output field
-            format_value = attributes.get('format', '')
-            format_value = f'format: {format_value}' if format_value else ''
-            if 'enum' in attributes:
-                enum_values = ', '.join(attributes['enum'])
-                format_value += (", " if format_value else "") + f'enum[{enum_values}]'
-            if 'minLength' in attributes:
-                min_length_value = str(attributes['minLength'])
-                format_value += (", " if format_value else "") + f'minLength={min_length_value}'
-            if 'maxLength' in attributes:
-                max_length_value = str(attributes['maxLength'])
-                format_value += (", " if format_value else "") + f'maxLength={max_length_value}'
-            if 'pattern' in attributes:
-                pattern_value = str(attributes['pattern'])
-                format_value += (", " if format_value else "") + f'pattern={pattern_value}'
-            if 'nullable' in attributes:
-                nullable_value = str(attributes['nullable'])
-                format_value += (", " if format_value else "") + f'nullable={nullable_value}'
+            temp_type = attributes.get('type')
+            if PRINT_TO_TERMINAL: print(f'Processing attribute: {parent_name}: {property_name}')
+            # if no attribute 'type' eg. a $ref, or not an object, or not an array of objects?
+            if temp_type is None or \
+                temp_type not in ('object') or \
+                (temp_type == 'array' and 'properties' not in attributes):     # and array properties not exist?
+                # combine various attribute definitions to the format output field
+                format_value = attributes.get('format', '')
+                format_value = f'format: {format_value}' if format_value else ''
+                if 'enum' in attributes:
+                    # enum_values = ', '.join(attributes['enum']) # to string for values that may be keywords
+                    format_value += (", " if format_value else "") + (f'enum{attributes["enum"]}')
+                    print(format_value)
+                if 'minLength' in attributes:
+                    min_length_value = str(attributes['minLength'])
+                    format_value += (", " if format_value else "") + f'minLength={min_length_value}'
+                if 'maxLength' in attributes:
+                    max_length_value = str(attributes['maxLength'])
+                    format_value += (", " if format_value else "") + f'maxLength={max_length_value}'
+                if 'pattern' in attributes:
+                    pattern_value = str(attributes['pattern'])
+                    format_value += (", " if format_value else "") + f'pattern={pattern_value}'
+                if 'nullable' in attributes:
+                    nullable_value = str(attributes['nullable'])
+                    format_value += (", " if format_value else "") + f'nullable={nullable_value}'
 
-            if '$ref' in attributes:
-                ref_value = attributes['$ref']
-                print(ref_value)
-                # if re.match(r'^https?://', ref_value):  # Check if the $ref is a URL
-                if not ref_value.startswith('#'):  # if $ref is not an inline/local reference, add the ref details
-                    fields.append({
-                        'Parent': parent_name if parent_name else "N/A",
-                        'Field': property_name + ': ' + ref_value,
-                        'Description': 'URL reference. Add schemas from referenced file to target file definition.',
-                        'Type': 'N/A',
-                        'Format': 'N/A',
-                        'Required': 'N/A',
-                        'Default': 'N/A',
-                        'Example': 'N/A'
-                    })
-                    continue
-                else:
-                    ref_name = ref_value.split('/')[-1]
-                    ref_schema = components['schemas'][ref_name]
-                    fields.extend(extract_fields_from_schema(ref_schema, components,
-                                                             f"{parent_name}.{property_name}" if parent_name else property_name))
-                    continue
+                if '$ref' in attributes:
+                    ref_value = attributes['$ref']
+                    print(ref_value)
+                    # if re.match(r'^https?://', ref_value):  # Check if the $ref is a URL
+                    if not ref_value.startswith('#'):  # if $ref is not an inline/local reference, add the ref details
+                        fields.append({
+                            'Parent': parent_name if parent_name else "N/A",
+                            'Field': property_name + ': ' + ref_value,
+                            'Description': 'Not a local reference. Add schemas from referenced file to target file definition.',
+                            'Type': 'N/A',
+                            'Format': 'N/A',
+                            'Required': 'N/A',
+                            'Default': 'N/A',
+                            'Example': 'N/A'
+                        })
+                        continue
+                    else:
+                        ref_name = ref_value.split('/')[-1]
+                        ref_schema = components['schemas'][ref_name]
+                        fields.extend(extract_fields_from_schema(ref_schema, components,
+                                                                f"{parent_name}.{property_name}" if parent_name else property_name))
+                        continue
 
-            field_data = {
-                'Parent': parent_name if parent_name else "N/A",
-                'Field': property_name,
-                'Description': attributes.get('description', 'N/A'),
-                'Type': attributes.get('type', 'N/A'),
-                'Format': format_value if format_value else 'N/A',
-                'Required': 'Yes' if property_name in required_properties else 'No',
-                'Default': attributes.get('default', 'N/A'),
-                'Example': attributes.get('example', 'N/A')
-            }
+                field_data = {
+                    'Parent': parent_name if parent_name else "N/A",
+                    'Field': property_name,
+                    'Description': attributes.get('description', 'N/A'),
+                    'Type': attributes.get('type', 'N/A'),
+                    'Format': format_value if format_value else 'N/A',
+                    'Required': 'Yes' if property_name in required_properties else 'No',
+                    'Default': attributes.get('default', 'N/A'),
+                    'Example': attributes.get('example', 'N/A')
+                }
 
-            fields.append(field_data)
+                fields.append(field_data)
 
             if attributes.get('type') == 'object':
                 fields.extend(extract_fields_from_schema(attributes, components,
@@ -141,7 +141,7 @@ def extract_fields_from_schema(schema, components, parent_name=''):
 
     return fields
 
-
+# Function to process each schema 
 def convert_schema_fields_to_matrix(components):
     schemas = components.get('schemas', {})
     all_fields = []
@@ -155,6 +155,7 @@ def convert_schema_fields_to_matrix(components):
     return all_fields
 
 
+#  Function to convert the fields list to CSV rows
 def format_as_csv(fields):
     output = StringIO()
     writer = csv.DictWriter(output, fieldnames=fields[0].keys())
@@ -163,9 +164,20 @@ def format_as_csv(fields):
         writer.writerow(field)
     return output.getvalue()
 
-
+# Function to write the CSV fields rows to file
 def write_csv_to_file(fields, csv_output_file):
     csv_output_file = csv_output_file + '.csv'
+    
+# try:
+#     with open('example.txt', 'w') as file:
+#         file.write("Hello, world!\n")
+# except PermissionError:
+#     print("You don't have permission to write to the file.")
+# except IOError:
+#     print("An I/O error occurred while writing to the file.")
+# except Exception as e:
+#     print(f"An error occurred: {e}")
+
     with open(csv_output_file, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fields[0].keys())
         writer.writeheader()
@@ -173,19 +185,30 @@ def write_csv_to_file(fields, csv_output_file):
             writer.writerow(field)
     print(f"Data written to {csv_output_file}")
 
+# class to set the alias pointers to not be included in YAML dump
+class NoAliasDumper(yaml.Dumper):
+    def ignore_aliases(self, data):
+        return True
+
 
 ############################################################################################
 ############################################################################################
+
 
 # The OpenApi specification YAML file to extract the properties/fields from.
-filename = 'EQL-portal-services-1.1.0-swagger-pre-resolved - extended defs.yaml'
+filename = 'EQL-nmi-aws-system-1.0.0-swagger - pre-resolved.yaml'
+# filename = 'EQL-portal-services-1.1.0-swagger-pre-resolved - extended defs.yaml'
 # test_spec_1.yaml Outage-1.0.0-swagger petstore simple2 EQL-portal-services-1.1.0-swagger
 
 # The folder where the spec file is location relative to the python interpreter
-spec_dir = '.\\specs\\'
+
+
+# log_file = f'{filename}-log.txt'
+# log_entries = []
+
 
 # Load both Swagger specification and data from a single file
-with open(spec_dir + filename, 'r') as data_file:
+with open(INPUT_DIR + filename, 'r') as data_file:
     data = yaml.safe_load(data_file)
 
 # Get the schemas
@@ -210,8 +233,65 @@ fields = convert_schema_fields_to_matrix(resolved_data_clean['components'])
 # Create a CSV data filename
 csv_data = format_as_csv(fields)
 
-for field in fields:
-    print(field)
+if PRINT_TO_TERMINAL:
+    for field in fields:
+        print(field)
 
 # convert the schema properties spec to CSV format and save to file
-write_csv_to_file(fields, filename)
+write_csv_to_file(fields, OUTPUT_DIR + filename)
+
+# TODO
+# bug with an object in an array - actually not, my bad with formatting excel file. DONE
+# logging
+# add parent array / repeatable type to indicate more than 1 instance of objects/elements
+# omit non data objects and arrays. DONE , not extensively tested.
+# tariffs and meters: array of strings , need to fix output. see example below. if items, get item.type, add as 'type: array(string)' or similar
+# have made an assumption that a referenced schema is not associated with other operations, this may be incorrect
+# handle all data attribute types
+# resolve refs in other local files
+# write exceptions etc to log file
+# dont output non referenced schemas
+# add exception handling, yaml parsing, file ops, field formatting, etc.
+# handle https, files ?
+# handle unreferenced schemas
+# parse ref'd files and insert into target
+# 
+# refs spec - Using $ref (swagger.io)
+# data types - Data Types (swagger.io)
+# OpenAPI-Specification/versions/3.0.0.md at main · OAI/OpenAPI-Specification · GitHub
+# 
+# tariffs:
+#     type: array
+#     items:
+#         type: string
+#     description: List of current Tariffs for that NMI. For detailed info about Services/Meters/Tariffs/PriceOptions call Get Meter API.
+#     example: ['8400', '7200', '9900'] 
+
+
+# def resolve_references(spec, root):
+#     """Recursively resolve references in the spec."""
+#     if isinstance(spec, dict):
+#         if '$ref' in spec:
+#             ref_value = spec['$ref']
+#             # Extract the key (e.g., MailingAddress from '#/components/schemas/MailingAddress')
+#             ref_key = ref_value.split('/')[-1]
+#             refs_encountered.add(ref_key)
+#             # Replace the current dict with the resolved ref
+#             return resolve_references(resolve_ref(root, ref_value), root)
+#         for key, value in spec.items():
+#             spec[key] = resolve_references(value, root)
+#     elif isinstance(spec, list):
+#         for index, item in enumerate(spec):
+#             spec[index] = resolve_references(item, root)
+#     return spec
+
+
+# try:
+#     with open('example.txt', 'w') as file:
+#         file.write("Hello, world!\n")
+# except PermissionError:
+#     print("You don't have permission to write to the file.")
+# except IOError:
+#     print("An I/O error occurred while writing to the file.")
+# except Exception as e:
+#     print(f"An error occurred: {e}")
